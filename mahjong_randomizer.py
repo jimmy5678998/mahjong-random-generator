8899b5c689cb15a46040c49ea99513f511fcc7dc
jimmy5678998 <67999796+jimmy5678998@users.noreply.github.com>
2020-07-15 01:59:58 +0800

    Add files via upload

---
+++ mahjong_randomizer.py
@@ -0,0 +1,20 @@
+import random
+
+
+def build_mountain():
+    pinzi_list = []
+    souzi_list = []
+    manzi_list = []
+    honor
+    for i in range(1, 10):
+        for a in range(0,4):
+            pinzi_list.append(i)
+            souzi_list.append(i)
+            manzi_list.append(i)
+    for i in range(1,8):
+        for a in range(0,4):
+
+
+
+    return
+build_mountain()
